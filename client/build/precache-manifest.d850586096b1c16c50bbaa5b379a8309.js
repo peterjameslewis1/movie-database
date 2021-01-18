882b5d734565fb9d0ef867efc0870ba018745f27
Peter Lewis <peterjameslewis4@hotmail.com>
2021-01-18 11:17:17 +0000

    initial deploy

---
+++ client/build/precache-manifest.d850586096b1c16c50bbaa5b379a8309.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af6ed666ccd7cb38df4b2ae34ebda02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86b817f8d00e6018a41e",
+    "url": "/static/css/2.04716422.chunk.css"
+  },
+  {
+    "revision": "3f1af3d0a063fbf4568e",
+    "url": "/static/css/main.5f76885a.chunk.css"
+  },
+  {
+    "revision": "86b817f8d00e6018a41e",
+    "url": "/static/js/2.20d3ddda.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.20d3ddda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f1af3d0a063fbf4568e",
+    "url": "/static/js/main.ac41b8d8.chunk.js"
+  },
+  {
+    "revision": "ad22bc4b00a2c265844c",
+    "url": "/static/js/runtime-main.42f2ecb5.js"
+  }
+]);
